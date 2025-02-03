19f3be65090ecb9611b87d8788ce315234a576f0
carrapatofa <github@alias.maximilianfabi.de>
2025-02-03 11:14:30 +0100

    changed segmentanytree path management to work with pipeline

--- run_docker_locally.sh
+++ run_docker_locally.sh
@@ -3,6 +3,15 @@
 CONTAINER_NAME="test_e2e_instance"
 IMAGE_NAME="nibio/e2e-instance"
 
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <source_path_1> <source_path_2>"
+    exit 1
+fi
+
+SOURCE_PATH_1=$1
+SOURCE_PATH_2=$2
+
 # Check if the container exists
 if [ $(docker container ls -a -q -f name=$CONTAINER_NAME) ]; then
     echo "Removing existing container $CONTAINER_NAME"
@@ -25,12 +34,25 @@ docker build -t $IMAGE_NAME .
 echo "Running the container"
 # docker run -it --gpus all --name $CONTAINER_NAME $IMAGE_NAME > e2e-instance.log 2>&1
 
-docker run -it --gpus all \
+#docker run -it --gpus all \
+#    --name $CONTAINER_NAME \
+#    --memory=100g \
+#    --memory-swap=100g \  # Prevent the container from swapping memory
+#    --oom-kill-disable \  # Allow the container to be killed if it exceeds the memory limit
+#    --mount type=bind,source=/home/teja/segmentanytree_in/,target=/home/nibio/mutable-outside-world/bucket_in_folder \
+#    --mount type=bind,source=/home/teja/segmentanytree_out/,target=/home/nibio/mutable-outside-world/bucket_out_folder \
+#    $IMAGE_NAME 
+
+#docker run -it --gpus all  \
+docker run -it --gpus '"device=0"' \
     --name $CONTAINER_NAME \
-    --mount type=bind,source=/home/nibio/mutable-outside-world/code/PanopticSegForLargeScalePointCloud_maciej/bucket_in_folder,target=/home/nibio/mutable-outside-world/bucket_in_folder \
-    --mount type=bind,source=/home/nibio/mutable-outside-world/code/PanopticSegForLargeScalePointCloud_maciej/bucket_out_folder,target=/home/nibio/mutable-outside-world/bucket_out_folder \
-    $IMAGE_NAME 
-
-
-
+    --memory=550g \
+    --memory-swap=550g \
+    --mount type=bind,source=$SOURCE_PATH_1,target=/home/nibio/mutable-outside-world/bucket_in_folder \
+    --mount type=bind,source=$SOURCE_PATH_2,target=/home/nibio/mutable-outside-world/bucket_out_folder \
+    $IMAGE_NAME
+    
+    #--memory=100g \
+    #--memory-swap=100g \
+    #--oom-kill-disable \
 
